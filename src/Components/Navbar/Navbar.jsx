06496e9780a3585892dfff906b2c6395abf7d256
surender18 <surenderempire68@gmail.com>
2024-11-04 00:06:47 +0530

    added react scroll

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-scroll'
 import './Navbar.css'
 import logo from '../../assets/logo1.png'
 import menu_icon from '../../assets/menu-icon.png'
@@ -24,12 +25,12 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav':''}`}>
         <img src={logo} alt="" className='logo' />
         <ul className={mobileMenu?'':'hide-mobile-menu'}>
-            <li >  Home</li>
-            <li>Program</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li>About us</li>
-            <li> <button className='btn'>Contact us</button></li>
+            <li><Link to='hero' smooth="true" offset={0} duration={500}>Home</Link> </li>
+            <li><Link to='program' smooth="true" offset={-280} duration={500}>Program</Link> </li>
+            <li><Link to='about' smooth="true" offset={-150} duration={500}>About us</Link> </li>
+            <li><Link to='campus' smooth="true" offset={-260} duration={500}>Campus</Link> </li>
+            <li><Link to='testimonials' smooth="true" offset={-260} duration={500}>Testimonials</Link> </li>
+            <li>  <Link to='contact' smooth="true" offset={-260} duration={500} className='btn'>Contact us</Link>  </li>
         </ul>
     <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
     </nav>
@@ -38,4 +39,3 @@ const Navbar = () => {
 
 export default Navbar
 
-//1:03:49
