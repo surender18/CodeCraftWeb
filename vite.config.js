640e21db03a8e5a733fc1d3510d542b8c9df2546
Aadish Jain <aadishjain017@gmail.com>
2024-11-04 16:28:13 +0530

    changed base path for build

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CodeCraftWeb',  // replace with your GitHub repo name
+  base: '/',  // replace with your GitHub repo name
 });
 
